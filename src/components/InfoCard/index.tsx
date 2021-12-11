668047e8ad152cc4208903b60eb48fbeb6c11981
Maicon <andrade.maicon.pr@gmail.com>
2021-12-10 21:25:07 -0300

    fix: bugfix infocard animation

--- src/components/InfoCard/index.tsx
+++ src/components/InfoCard/index.tsx
@@ -45,9 +45,9 @@ export function InfoCard({ title, data, target }:IProps) {
         }}
         transition={{
           ease: 'easeInOut',
-          opacity: { duration: 1 },
-          x: { duration: 0.3 },
-          y: { duration: 0.3 }
+          opacity: { duration: 0 },
+          x: { duration: 0 },
+          y: { duration: 0 }
         }}
       >
         <div className="infoCard-content">
